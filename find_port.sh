c2b559f601ce001b17c601c218d9efe6afdd54cc
feiyu <304719651@qq.com>
2017-11-25 20:24:55 +0800

    扫描主机端口状态

---
+++ find_port.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+HOST=$1
+PORT="22 25 80 8080"
+for PORT in $PORT; do
+    if echo &>/dev/null > /dev/tcp/$HOST/$PORT; then
+        echo "$PORT open"
+    else
+        echo "$PORT close"
+    fi
+done
