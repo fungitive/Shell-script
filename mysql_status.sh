772b2cbb1370b22260c3b1ec535e030a221ac7cb
feiyu <304719651@qq.com>
2017-11-25 19:28:51 +0800

    检查MySQL主从同步状态

---
+++ mysql_status.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+USER=bak
+PASSWD=123456
+IO_SQL_STATUS=$(mysql -u$USER -p$PASSWD -e 'show slave status\G' |awk -F: '/Slave_.*_Running/{gsub(": ",":");print $0}')  #gsub去除冒号后面的空格
+for i in $IO_SQL_STATUS; do
+    THREAD_STATUS_NAME=${i%:*}
+    THREAD_STATUS=${i#*:}
+    if [ "$THREAD_STATUS" != "Yes" ]; then
+        echo "Error: MySQL Master-Slave $THREAD_STATUS_NAME status is $THREAD_STATUS!"
+    fi
+done
