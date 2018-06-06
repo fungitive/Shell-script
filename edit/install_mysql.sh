5434e10e964d9e5f75a3e557f5e0020779cd84a6
feiyu <304719651@qq.com>
2018-06-06 13:53:53 +0800

    6.6.

--- edit/install_mysql.sh
+++ edit/install_mysql.sh
@@ -4,6 +4,10 @@ yum remove mariadb-libs -y
 # install Environment with yum
 yum -y install make cmake bison-devel ncurses-devel gcc gcc-c++ kernel-devel &&\
 yum install -y readline-devel pcre-devel openssl-devel openssl zlib zlib-devel pcre-devel  perl perl-devel wget
+#install boost_1_59_0
+wget -c https://dl.bintray.com/boostorg/release/1.59.0/source/boost_1_59_0.tar.gz
+tar -xzvf boost_1_59_0.tar.gz && cp boost_1_59_0 /usr/local/
+#install mysql
 wget -c https://cdn.mysql.com/archives/mysql-5.7/mysql-5.7.22.tar.gz &&\
 tar -zxvf mysql-5.7.22.tar.gz && rm -f mysql-5.7.22.tar.gz && cd mysql-5.7.22 &&\
 #make datadir ,homedir and backupdir
@@ -12,23 +16,6 @@ mkdir -p /home/mysql/data  /usr/local/mysql /backup
 groupadd mysql && useradd -g mysql mysql
 chown -R mysql:mysql /home/mysql/data /backup
 # cmake
-cmake \
--DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
--DMYSQL_DATADIR=/mysql/data \
--DSYSCONFDIR=/etc \
--DWITH_MYISAM_STORAGE_ENGINE=1 \
--DWITH_INNOBASE_STORAGE_ENGINE=1 \
--DWITH_MEMORY_STORAGE_ENGINE=1 \
--DWITH_READLINE=1 \
--DMYSQL_UNIX_ADDR=/var/lib/mysql/mysql.sock \
--DMYSQL_TCP_PORT=3306 \
--DENABLED_LOCAL_INFILE=1 \
--DWITH_PARTITION_STORAGE_ENGINE=1 \
--DEXTRA_CHARSETS=all \
--DDEFAULT_CHARSET=utf8 \
--DDEFAULT_COLLATION=utf8_general_ci \
--DLOWER_CASE_TABLE_NAMES=1
-
 cmake . -DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
 -DMYSQL_DATADIR=/var/lib/mysql -DSYSCONFDIR=/etc \
 -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_ARCHIVE_STORAGE_ENGINE=1 \
@@ -37,30 +24,23 @@ cmake . -DCMAKE_INSTALL_PREFIX=/usr/local/mysql \
 -DENABLED_LOCAL_INFILE=1 -DWITH_EXTRA_CHARSETS=all \
 -DMYSQL_USER=mysql -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/usr/local/boost_1_59_0
 make && make install
-#mv /etc/my.cnf if exists it will be failure
-mv /etc/my.cnf /etc/my.cnfbk
-#install mysqldb and start mysql server
-/usr/local/mysql/scripts/mysql_install_db --basedir=/usr/local/mysql --datadir=/mysql/data --user=mysql
+#Configure Mysql
 cp /usr/local/mysql/support-files/mysql.server /etc/init.d/mysql
+mkdir /var/lib/mysql && chown mysql. /var/lib/mysql
+echo -e '[mysqld] \nbasedir = /usr/local/mysql \ndatadir = /var/lib/mysql \nport = 3306 \nsocket = /tmp/mysqld.sock' > /etc/my.cnf
+#mysql_install_db 被废弃了，取而代之的是 mysqld –initialize
+/usr/local/mysql/bin/mysqld --defaults-file=/etc/my.cnf --initialize  --datadir=/var/lib/mysql --basedir=/usr/local/mysql --user=mysql
+systemctl start mysql
+sed i 's/^PATH=$PATH:$HOME/bin/PATH=$PATH:$HOME/bin:/usr/local/mysql/bin/'  ~/.bash_profile
+ln -s /usr/local/mysql/bin/mysql /usr/bin
+#start mysql
+chkconfig --add mysqld &&systemctl start mysql
 chkconfig mysql on
-service mysql start
+
 # change the user of database
-/usr/local/mysql/bin/mysql -e"grant all on *.* to 'root'@'%' identified by '123456';"
-/usr/local/mysql/bin/mysql -e"grant all on *.* to 'root'@localhost identified by '123456';"
-/usr/local/mysql/bin/mysql -uroot -p123456 -e"delete from mysql.user where password='';"
+#/usr/local/mysql/bin/mysql -e"grant all on *.* to 'root'@'%' identified by '123456';"
+#/usr/local/mysql/bin/mysql -e"grant all on *.* to 'root'@localhost identified by '123456';"
+#/usr/local/mysql/bin/mysql -uroot -p123456 -e"delete from mysql.user where password='';"
 # default my.cnf /usr/local/mysql/my.cnf
 
 
-#Configure Mysql
-\cp /usr/local/mysql/share/mysql/my-medium.cnf  /etc/my.cnf && \cp /usr/local/mysql/share/mysql/mysql.server /etc/init.d/mysqld &&  chkconfig --add mysqld && chkconfig --level 345 mysqld on || echo -e '\e[31mMysql Server Configuue Failure!\e[0m' exit 2
-#Useradd mysql user
-id mysql>/dev/null 2>&1 || useradd mysql
-cd /usr/local/mysql
-chown -R mysql.mysql /usr/local/mysql && /usr/local/mysql/bin/mysql_install_db --user=mysql > /dev/null 2>&1 &&
-chown -R mysql var && /usr/local/mysql/bin/mysqld_safe --user=mysql& > /dev/null 2>&1 &&
-if [ $? -eq 0 ];then
-echo -e '\e[32mMysql Server Configure Success!\e[0m'
-else
-echo -e '\e[31mMysql Server Configuue Failure!\e[0m'
-exit 1
-fi
